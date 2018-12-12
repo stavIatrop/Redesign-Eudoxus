0a04c44e54ea1f4cd442c05d2e510db196f5608d
Mike Xydas <mikevin97@windowslive.com>
2018-12-12 12:51:27 +0200

    Tested bootstrap, php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php echo "PHP is working !"; ?>
+<head>
+	<link href="css/bootstrap.min.css" rel="stylesheet"/>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+</html>
+	<button type="button" class="btn btn-success btn-lg">Success</button>
+	<?php 
+		echo "Hello world!"; 
+	?>
+</body>
