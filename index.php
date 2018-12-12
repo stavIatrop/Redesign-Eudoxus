88c169e8a77e5879737458936a69a8935aa1eec1
Mike Xydas <mikevin97@windowslive.com>
2018-12-12 13:38:00 +0200

    Added simple grid example

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <head>
 	<link href="css/bootstrap.min.css" rel="stylesheet"/>
+	<link rel="stylesheet" type="text/css" href="css/gridLayouts.css">
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-</html>
-	<button type="button" class="btn btn-success btn-lg">Success</button>
-	<?php 
-		echo "Hello world!"; 
-	?>
+  <div class="container-fluid">
+    <div class="row topPart">
+      <div class="col">
+        1 of 2
+      </div>
+    </div>
+    <div class="row indexDims">
+      <div class="col-md-3">
+        1 of 2
+      </div>
+      <div class="col">
+        2 of 2
+      </div>
+    </div>
+  </div>
 </body>
