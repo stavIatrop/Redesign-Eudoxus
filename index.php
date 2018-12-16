d5e3a33db73d5946e48064b4a2d4046e532e7cb4
Mike Xydas <mikevin97@windowslive.com>
2018-12-16 11:54:20 +0200

    Created info contaienr of Homepage (in progress)

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <head>
 	<link href="css/bootstrap.min.css" rel="stylesheet"/>
-	<link rel="stylesheet" type="text/css" href="css/gridLayouts.css">
-	<script src="js/bootstrap.min.js"></script>
+	<link href="css/gridLayouts.css" rel="stylesheet" type="text/css"  media="all">
+  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"></head>
 </head>
 <body>
   <div class="container-fluid">
@@ -15,7 +16,31 @@
         1 of 2
       </div>
       <div class="col">
-        2 of 2
+        <div class="row infoContainer">
+          <div class="col-md-2">
+            <div class="row" style="height:25%">
+              <button class="infoButton btn btn-outline-dark rounded-0 active"><i class="fas fa-graduation-cap"></i>    Φοιτητής</button>
+            </div>
+            <div class="row" style="height:25%">
+              <button class="infoButton btn btn-outline-dark rounded-0"><i class="fas fa-book"></i>    Εκδότης</button>
+            </div>
+            <div class="row" style="height:25%">
+              <button class="infoButton btn btn-outline-dark rounded-0"><i class="fas fa-landmark"></i>    Γραμματεία</button>
+            </div>
+            <div class="row" style="height:25%">
+              <button class="infoButton btn btn-outline-dark rounded-0"><i class="fas fa-truck"></i>    <span>Σημείο<br>Διανομής</span></button>
+            </div>
+          </div>
+          <div class="col-md-10">
+            <p class=" pull-right infoTitle">Ο φοιτητής μπορεί να:</p>
+            <ul style="padding-left: 15%;">
+              <li class="infoAbility">πραγματοποιήσει <a href="url">δήλωση συγγραμμάτων</a></li>
+              <li class="infoAbility">μεταβεί στην  <a href="url">τροποποίηση τρέχουσας δήλωσης</a></li>
+              <li class="infoAbility"><a href="url">αναζητήσει</a> συγγράμματα</li>
+            </ul>
+            <p class="infοDeadline">Προθεσμία Δηλώσεων Συγγραμμάτων: 17/11/2018</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
