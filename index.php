d3291f6738b5932253d98f394535fa8024edc73b
Mike Xydas <mikevin97@windowslive.com>
2018-12-16 12:12:22 +0200

    Fixed future confilcts

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 	<link href="css/gridLayouts.css" rel="stylesheet" type="text/css"  media="all">
   <script src="js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"></head>
+  <link rel="stylesheet" type="text/css" href="css/carouselIndicators.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
   <div class="container-fluid">
@@ -13,7 +15,57 @@
     </div>
     <div class="row indexDims">
       <div class="col-md-3">
-        1 of 2
+         <!--Carousel Wrapper-->
+         <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+          <!--Indicators-->
+          <ol class="carousel-indicators pointer">
+            <li data-target="#carousel-example-1z" data-slide-to="0" class="active" ></li>
+            <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+            <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+          </ol>
+          <!--/.Indicators-->
+          <!--Slides-->
+          <div class="carousel-inner" role="listbox">
+            <!--First slide-->
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="https://get.pxhere.com/photo/landscape-nature-rock-sky-night-star-adventure-view-atmosphere-relax-darkness-galaxy-night-sky-moonlight-outer-space-mountain-landscape-mountaineering-astronomy-mountains-starry-sky-fantasy-high-mountains-astronomical-object-geological-phenomenon-1286368.jpg" alt="First slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Προθεσμία Δήλωσης Συγγραμμάτων Χειμερινού Εξαμήνου</h5>
+                <p>Παρασκευή 21 Δεκεμβρίου 2018</p>
+              </div>
+            </div>
+            <!--/First slide-->
+            <!--Second slide-->
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://www.bryanhanna.com/wp-content/uploads/2013/02/MG_2411-Edit-3.jpg" alt="Second slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Προθεσμία Παραλαβής Συγγραμμάτων Χειμερινού Εξαμήνου</h5>
+                <p>Παρασκευή 11 Ιανουαρίου 2019</p>
+              </div>
+            </div>
+            <!--/Second slide-->
+            <!--Third slide-->
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://get.pxhere.com/photo/people-sky-night-star-milky-way-dawn-atmosphere-idyllic-evening-scenic-scenery-darkness-galaxy-night-sky-aurora-moonlight-outer-space-outdoors-nighttime-astronomy-stars-universe-starry-sky-beautiful-backlit-silhouettes-midnight-astronomical-object-star-gazing-geological-phenomenon-1177774.jpg" alt="Third slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Καληνύχτα!!</h5>
+              </div>
+            </div>
+            <!--/Third slide-->
+          </div>
+          <!--/.Slides-->
+          
+          <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+          
+        </div>
+
       </div>
       <div class="col">
         <div class="row infoContainer">
