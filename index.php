7dab240d77e6f5eea23dd5ec59e6aeeb27e5cdd9
Mike Xydas <mikevin97@windowslive.com>
2018-12-17 11:53:02 +0200

    Created top part of website

--- index.php
+++ index.php
@@ -15,7 +15,31 @@
     <div class="row topPart">
       <div class="col">
         <div class="row topTopPart">
-          <div>SubRow1</div>
+          <div class="col-md-1">
+            <a href="homepageUrl"><img class="logo" src="images/logo.png" alt="Logo"></a>
+          </div>
+          <div class="col-md-8">
+            <div class="row" style="height: 50%;">
+              <div class="col">
+                <p class="tagline">Σύντομη πρόταση περιγραφής του ιστοχώρου</p>
+              </div>
+            </div>
+            <div class="row" style="height: 50%;">
+              <div class="col">
+                <div style="margin-left:5%;"><input type="text" class="searchBar" placeholder="Αναζήτησε πληροφορίες, τίτλο συγγράμματος, συγγραφέα..."><button style="height:55%; margin-left:1%;" class="btn btn-dark"> Αναζήτηση </button></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <a href="Login"><i class="fas fa-sign-in-alt topIcons"></i><p class="loginText">Είσοδος / Εγγραφή</p></a>
+          </div>
+          <div class="col-md-1">
+            <a href="Login"><i style="margin-left: 29%;" class="fas fa-user topIcons"></i><p class="loginText">Προφίλ</p></a>
+          </div>
+          <div class="col-md-1">
+            <a href="greekVersion"><img class="flag" src="images/greek.png" alt="Greek flag"></a>
+            <a href="englishVersion"><img class="flag" src="images/english.png" alt="Greek flag"></a>
+          </div>
         </div>
         <div class="row navBarRow">
           <div>SubRow2</div>
