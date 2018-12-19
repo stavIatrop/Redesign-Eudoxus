1a9e8935d18d8cce35c3817b3932e357e407ded0
Mike Xydas <mikevin97@windowslive.com>
2018-12-19 12:42:55 +0200

    Created breadcrumb

---
+++ getbooks.php
@@ -0,0 +1,158 @@
+<head>
+	<link href="css/bootstrap.min.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"></head>
+  <link href="css/gridLayouts.css" rel="stylesheet" type="text/css"  media="all">
+  <link href="css/topPart.css" rel="stylesheet" type="text/css"  media="all">
+  <link href="css/navBar.css" rel="stylesheet" type="text/css"  media="all">
+  <link href="css/getbooks.css" rel="stylesheet" type="text/css"  media="all">
+  <link href="css/footer.css" rel="stylesheet" type="text/css"  media="all">
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  
+  
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- <script src="js/homepage.js"></script> -->
+
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row topPart">
+      <div class="col-md-12">
+        <div class="row topTopPart">
+          <div class="col-md-1">
+            <a class="logoDiv" href="homepageUrl"><img class="logo" src="images/logo.png" alt="Logo"><p class="logoText">Εύδοξος</p></a>
+          </div>
+          <div class="col-md-8">
+            <div class="row" style="height: 50%;">
+              <div class="col">
+                <p class="tagline">Σύντομη πρόταση περιγραφής του ιστοχώρου</p>
+              </div>
+            </div>
+            <div class="row" style="height: 50%;">
+              <div class="col">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <a class="iconText" href="Login"><i class="fas fa-sign-in-alt topIcons"></i><p class="loginText">Είσοδος / Εγγραφή</p></a>
+          </div>
+          <div class="col-md-1">
+            <a class="iconText" href="Login"><i style="margin-left: 29%;" class="fas fa-user topIcons"></i><p class="loginText">Προφίλ</p></a>
+          </div>
+          <div class="col-md-1">
+            <a href="greekVersion"><img class="flag rounded" src="images/greek.png" alt="Greek flag"></a>
+            <a href="englishVersion"><img class="flag rounded" src="images/english.png" alt="Greek flag"></a>
+          </div>
+        </div>
+        <div class="row navBarRow">
+          <div class="col-md-12">
+            <nav style="" class="navbar rounded navbar-expand-lg">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Αρχική</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Φοιτητής
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Δήλωση Συγγραμμάτων</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Αναζήτηση Συγγραμμάτων</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Επισκόπηση Δήλωσης</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Βοήθεια</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Εκδότης
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Καταχώρηση Συγγράμματος</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Διαχείρηση Συγγραμμάτων</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Βοήθεια</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Γραμματεία
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Σημεια Διανομής
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link " href="#">Επικοινωνία</a>
+                </li>
+              </ul>
+              <form action="#" method="post" class="form-inline my-2 mylg-0">
+                <input type="search" name="search" id="search" class="form-control mr-sm-2" placeholder="Τίτλος, πληροφορίες..." aria-label="Search">
+                <button class="btn btn-dark" type="submit">Αναζήτηση</button>
+              </form>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row breadcrumb">
+      <div class="col-md-12">
+        <ul class="breadcrumbT">
+          <li><a href="#">Αρχική</a></li>
+          <li><a href="#">Δήλωση Συγγραμμάτων</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <footer class="section footer-classic context-dark bg-image" style="background: #E7E7E7;">
+        <div class="container">
+          <div style="padding-top:5%; padding-bottom: 3%;" class="row row-30">
+            <div style="border-right: 1px solid grey;" class="col-md-3 col-xl-4">
+              <div class="pr-xl-3">
+                <p class="footerHeader">Εύδοξος</p>
+                <!-- Rights-->
+                <p class="rights"><span>©  </span><span class="copyright-year">2018</span><span> </span><span>Stavroula Iatropoulou, Mike Xydas</span><span>. </span><p>All Rights Reserved.</p></p>
+              </div>
+            </div>
+            <div style="border-right: 1px solid grey; margin-left: 6%;" class="col-md-3">
+              <h5>Επικοινωνία</h5>
+              <dl class="contact-list">
+                <dt>email επικοινωνίας:</dt>
+                <dd><a href="mailto:#">help@eudoxos.com</a></dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>Τηλέφωνο:</dt>
+                <dd><a href="tel:#">210-6711223</a>
+                </dd>
+              </dl>
+            </div>
+            <div style="margin-left: 5%;" class="col-md-4 col-xl-3">
+              <h5>Σύνδεσμοι</h5>
+              <ul class="nav-list">
+                <li><a href="#">Φοιτητής</a></li>
+                <li><a href="#">Εκδότης</a></li>
+                <li><a href="#">Γραμματεία</a></li>
+                <li><a href="#">Σημεία Διανομής</a></li>
+                <li><a href="#">Πληροφορίες</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+</body>
